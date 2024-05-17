96526f249d4ddf46c631072a40e8065b4f99ce91
wernerwxxx7547 <tsp1rejestr@vp.pl>
2024-05-17 21:44:51 +0200

    pop4b

--- MotoApp/Entities/Menager.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MotoApp.Entities;
-
-public class Menager : EntityBase
-{
-    public string? FirstName { get; set; }
-    public override string ToString() => $"Id: {Id}, Name: {FirstName}, Wartość: {num} ";
-    //public override string ToString() => base.ToString() + " (Metale Szlachetne)" + $" Wartość: {num}";
-}
